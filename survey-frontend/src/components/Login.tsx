378f901314fde6642da2ec3bc3938483d4eb298f
bagas <bagaskalihputra.bkp@gmail.com>
2024-11-15 00:09:30 +0700

    style(frontend): better ui

--- survey-frontend/src/components/Login.tsx
+++ survey-frontend/src/components/Login.tsx
@@ -1,65 +1,95 @@
 import React, { useState } from "react";
-import { TextField, Button, Typography, Container } from "@mui/material";
+import { Box, TextField, Button, Typography } from "@mui/material";
 import { login } from "../api/auth";
 import { AuthData } from "../types/auth";
 
-const Login: React.FC = () => {
+const Login = () => {
   const [formData, setFormData] = useState<AuthData>({
     username: "",
     password: "",
   });
-  const [message, setMessage] = useState<string>("");
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
-  const handleSubmit = async (e: React.FormEvent) => {
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     try {
       const response = await login(formData);
-      setMessage(`Login successful! Token: ${response.token}`);
+      console.log(response);
     } catch (error) {
       console.error(error);
-      setMessage("Login failed");
     }
   };
 
   return (
-    <Container maxWidth="xs">
-      <Typography variant="h4" gutterBottom>
-        Login
-      </Typography>
-      <form onSubmit={handleSubmit}>
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        minHeight: "100vh",
+      }}
+    >
+      <Box
+        component="form"
+        onSubmit={handleSubmit}
+        sx={{
+          mt: 3,
+          border: "1px solid black",
+          display: "flex",
+          width: "25%",
+          padding: "60px 20px",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Typography variant="h4" gutterBottom>
+          Sign In
+        </Typography>
         <TextField
+          variant="outlined"
+          margin="normal"
+          required
           fullWidth
+          id="username"
           label="Username"
           name="username"
+          autoComplete="username"
+          autoFocus
           value={formData.username}
           onChange={handleChange}
-          required
-          margin="normal"
         />
         <TextField
+          variant="outlined"
+          margin="normal"
+          required
           fullWidth
-          label="Password"
           name="password"
+          label="Password"
           type="password"
+          id="password"
+          autoComplete="current-password"
           value={formData.password}
           onChange={handleChange}
-          required
-          margin="normal"
         />
-        <Button fullWidth type="submit" variant="contained" color="primary">
-          Login
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          color="primary"
+          sx={{ mt: 3, mb: 2 }}
+        >
+          Sign In
         </Button>
-      </form>
-      {message && (
-        <Typography variant="body1" color="textSecondary">
-          {message}
-        </Typography>
-      )}
-    </Container>
+      </Box>
+      <Typography variant="body1" sx={{ mt: 3 }} color="textSecondary">
+        Don't have an account? <a href="/register">Sign Up</a>
+      </Typography>
+    </Box>
   );
 };
 
--- survey-frontend/src/components/Register.tsx
+++ survey-frontend/src/components/Register.tsx
@@ -1,21 +1,22 @@
 import React, { useState } from "react";
-import { TextField, Button, Typography, Container } from "@mui/material";
+import { Box, TextField, Button, Typography } from "@mui/material";
 import { register } from "../api/auth";
 import { AuthData } from "../types/auth";
 
-const Register: React.FC = () => {
+const RegisterPage = () => {
   const [formData, setFormData] = useState<AuthData>({
     username: "",
     password: "",
     role: "USER",
   });
+
   const [message, setMessage] = useState<string>("");
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
-  const handleSubmit = async (e: React.FormEvent) => {
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     try {
       await register(formData);
@@ -27,41 +28,78 @@ const Register: React.FC = () => {
   };
 
   return (
-    <Container maxWidth="xs">
-      <Typography variant="h4" gutterBottom>
-        Register
-      </Typography>
-      <form onSubmit={handleSubmit}>
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        minHeight: "100vh",
+      }}
+    >
+      <Box
+        component="form"
+        onSubmit={handleSubmit}
+        sx={{
+          mt: 3,
+          border: "1px solid black",
+          display: "flex",
+          width: "25%",
+          padding: "60px 20px",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Typography variant="h4" gutterBottom>
+          Sign Up
+        </Typography>
         <TextField
+          variant="outlined"
+          margin="normal"
+          required
           fullWidth
+          id="username"
           label="Username"
           name="username"
+          autoComplete="username"
+          autoFocus
           value={formData.username}
           onChange={handleChange}
-          required
-          margin="normal"
         />
         <TextField
+          variant="outlined"
+          margin="normal"
+          required
           fullWidth
-          label="Password"
           name="password"
+          label="Password"
           type="password"
+          id="password"
+          autoComplete="current-password"
           value={formData.password}
           onChange={handleChange}
-          required
-          margin="normal"
         />
-        <Button fullWidth type="submit" variant="contained" color="primary">
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          color="primary"
+          sx={{ mt: 3, mb: 2 }}
+        >
           Register
         </Button>
-      </form>
+      </Box>
       {message && (
         <Typography variant="body1" color="textSecondary">
           {message}
         </Typography>
       )}
-    </Container>
+      <Typography variant="body1" color="textSecondary">
+        Already have an account? <a href="/login">Login</a>
+      </Typography>
+    </Box>
   );
 };
 
-export default Register;
+export default RegisterPage;
