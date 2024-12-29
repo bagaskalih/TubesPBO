cac20fa54d509fb1267eb29c7a1d62058c049e01
Zikra2244 <zikragaul12321@gmail.com>
2024-12-29 07:03:02 +0700

    chore(BE): add data in DatabaseSeeder to initialize survey categories if none exist

--- survey-backend/src/main/java/com/kelompok2/survey_backend/DatabaseSeeder.java
+++ survey-backend/src/main/java/com/kelompok2/survey_backend/DatabaseSeeder.java
@@ -1,20 +1,28 @@
 package com.kelompok2.survey_backend;
 
+import com.kelompok2.survey_backend.model.SurveyCategory;
 import com.kelompok2.survey_backend.model.User;
+import com.kelompok2.survey_backend.repositories.SurveyCategoryRepository;
 import com.kelompok2.survey_backend.repositories.UserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Component
 public class DatabaseSeeder implements CommandLineRunner {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final SurveyCategoryRepository surveyCategoryRepository;
 
-    public DatabaseSeeder(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    public DatabaseSeeder(UserRepository userRepository, PasswordEncoder passwordEncoder, SurveyCategoryRepository surveyCategoryRepository) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
+        this.surveyCategoryRepository = surveyCategoryRepository;
     }
 
     @Override
@@ -31,6 +39,18 @@ public class DatabaseSeeder implements CommandLineRunner {
             user.setPassword(passwordEncoder.encode("user123"));
             user.setRole("USER");
             userRepository.save(user);
+        }
+
+        if (surveyCategoryRepository.count() == 0) {
+            List<SurveyCategory> categories = Arrays.asList(
+                    new SurveyCategory(null, "Pengetahuan Umum", new ArrayList<>()),
+                    new SurveyCategory(null, "Sejarah", new ArrayList<>()),
+                    new SurveyCategory(null, "Matematika", new ArrayList<>()),
+                    new SurveyCategory(null, "Ekonomi", new ArrayList<>()),
+                    new SurveyCategory(null, "Trivia", new ArrayList<>())
+            );
+
+            surveyCategoryRepository.saveAll(categories);
         }
     }
 }
